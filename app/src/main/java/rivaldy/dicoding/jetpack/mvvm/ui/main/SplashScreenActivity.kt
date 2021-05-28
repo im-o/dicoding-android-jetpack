9c79bac161c3ab02829d127a6ec678c53ce787f6
im-o <frizgt@gmail.com>
2021-05-29 00:40:20 +0800

    :zap: add splash screen

---
+++ app/src/main/java/rivaldy/dicoding/jetpack/mvvm/ui/main/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package rivaldy.dicoding.jetpack.mvvm.ui.main
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import rivaldy.dicoding.jetpack.mvvm.R
+import rivaldy.dicoding.jetpack.mvvm.utils.UtilExtensions.openActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(mainLooper).postDelayed({
+            openActivity(MainActivity::class.java)
+            finish()
+        }, 2000)
+    }
+}
